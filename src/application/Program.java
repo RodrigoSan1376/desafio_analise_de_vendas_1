f3a0c81b275557ed5471055faeb2c75ad7d4011e
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2022-10-27 20:20:55 -0300

    Implemented IOException to the file path

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,7 @@ import entities.Sale;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
